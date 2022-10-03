71185f5157fcacfb05346ca7941c8c3d3c064d80
poodraju <114132349+poodraju@users.noreply.github.com>
2022-10-03 11:00:53 -0700

    Add files via upload

--- registation.js
+++ registation.js
@@ -1,11 +1,15 @@
+'use strict'
+const submit = document.getElementById('submit');
+const inputfield = document.getElementsByClassName('raju')
 
-const submit = document.getElementById('button');
-const inputfield = document.getElementsByClassName('name')
+console.log(submit)
 
 submit.onclick = async() => {
+    console.log('button clicked')
+    console.log(inputfield[0].value)
     const name = inputfield[0].value;
 
-    const res=await fatch('https://api.agify.io?name=?${name}',
+    const res=await fetch(`https://api.agify.io?name=${name}`,//backtic(`)its show in down os esc button//
     {method:'GET', redirect:'follow'})
     .then(response => response.json())
     .then(result =>{
@@ -13,6 +17,7 @@ submit.onclick = async() => {
     })
     .catch(error => console.log('error',error));
     const cardTitele = document.getElementsByClassName('card-title')[0]
-    cardTitele.innerHTML=name.toupperCass();
-    document.getElementsByClassName('card-text')[0].innerHTML = 'The age of $ {name} is $ {res}'
+    cardTitele.innerHTML=name.toUpperCase();
+    document.getElementsByClassName('card-text')[0].innerHTML = `The age of ${name} is ${res.age}`
+    console.log(res)
 }
